918fc1001aaed241f8eb24eb0ba5a5b30a42c4e2
Colby <okeefeca@icloud.com>
2024-07-20 14:42:59 -0400

    Update GameManager.cs

--- Core/GameManager.cs
+++ Core/GameManager.cs
@@ -65,8 +65,7 @@ namespace PlazmaGames.Core
 			if (_instance) return;
 
 			PlazmaGamesSettings settings = PlazmaGamesSettings.GetSettings();
-//            string prefabPath = settings.GetSceneGameManagerNameOrDefault(SceneManager.GetActiveScene().name);
-			string prefabPath = "GameManager";
+            		string prefabPath = settings.GetSceneGameManagerNameOrDefault(SceneManager.GetActiveScene().name);
 
 			GameManager gameManagerPrefab = Resources.Load<GameManager>(prefabPath);
 			GameManager gameManager = Instantiate(gameManagerPrefab);
