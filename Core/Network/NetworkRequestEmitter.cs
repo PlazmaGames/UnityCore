51e0f8bafc35946392e04621c5b733b0972fbe4c
Colby O'Keefe <okeefeca@icloud.com>
2024-09-04 15:00:49 -0400

    WhiteSpaceIssue

--- Core/Network/NetworkRequestEmitter.cs
+++ Core/Network/NetworkRequestEmitter.cs
@@ -4,11 +4,11 @@ using System.Linq;
 
 namespace PlazmaGames.Core.Network
 {
-    internal sealed class NetworkRequestEmitter
+	internal sealed class NetworkRequestEmitter
 	{
 		Dictionary<int, (List<(PacketReader, int)>, List<Action<PacketReader, int>>)> _events = new Dictionary<int, (List<(PacketReader, int)>, List<Action<PacketReader, int>>)>();
 
-        public void Emit(int type, PacketReader val, int fromID)
+       		public void Emit(int type, PacketReader val, int fromID)
 		{
 			if (!_events.ContainsKey(type)) return;
 			_events[type].Item1.Add((val, fromID));
@@ -22,20 +22,20 @@ namespace PlazmaGames.Core.Network
 
 		public void Unsubscribe(int type, Action<PacketReader, int> callback)
 		{
-            if (!_events.ContainsKey(type)) return;
-            _events[type].Item2.Remove(callback);
-        }
+	            if (!_events.ContainsKey(type)) return;
+	            _events[type].Item2.Remove(callback);
+	        }
 
 		public void CheckForRequest()
 		{
-            foreach ((List<(PacketReader, int)>, List<Action<PacketReader, int>>) data in _events.Values.ToList())
+		        foreach ((List<(PacketReader, int)>, List<Action<PacketReader, int>>) data in _events.Values.ToList())
 			{
-                for (int i = data.Item1.Count - 1; i >= 0; i--)
-                {
-                    foreach (Action<PacketReader, int> act in data.Item2) act.Invoke(new PacketReader(data.Item1[i].Item1.GetPacket()), data.Item1[i].Item2);
-                    data.Item1.RemoveAt(i);
-                }
-            }
-        }
+		                for (int i = data.Item1.Count - 1; i >= 0; i--)
+		                {
+		                    foreach (Action<PacketReader, int> act in data.Item2) act.Invoke(new PacketReader(data.Item1[i].Item1.GetPacket()), data.Item1[i].Item2);
+		                    data.Item1.RemoveAt(i);
+		                }
+	                }
+	        }
 	}
 }
